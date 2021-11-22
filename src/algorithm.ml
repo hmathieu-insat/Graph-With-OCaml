5457917c5b877fedb83b0aee9c2d1a91be05ea17
HeineKayn <thomashoger@hotmail.fr>
2021-11-22 11:46:04 +0100

    find_path
    debut

--- src/algorithm.ml
+++ src/algorithm.ml
@@ -2,4 +2,32 @@ open Graph
 open Gfile
 open Tools
 
+(* let find_path gr forbidden id1 id2 =
+  let arcs = out_arcs gr id1 in
 
+  let rec loop arcs forbidden path =
+    match arcs with 
+      | (id,_) when id=id2 -> Some(id2::path)
+      | [] -> forbidden
+      | (id,_)::reste -> loop reste (id::forbidden) (id::path)
+  in
+
+  loop arcs forbidden []
+  (* find_path *)
+;;
+   *)
+
+let rec find_path gr forbidden id1 id2 path =
+  let arcs = out_arcs gr id1 in
+  
+    let rec loop arcs forbidden path =
+      match arcs with 
+        | (id,_) when id=id2 -> Some(id2::path)
+        | [] -> forbidden
+        | (id,_)::reste -> loop reste (id::forbidden) (id::path)
+    in
+  
+    loop arcs forbidden path
+    (* find_path *)
+  ;;
+    
