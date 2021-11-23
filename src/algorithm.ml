8fc902cd3f9a6745b0940dbc6854be2fcc6bcf9f
Hugo Mathieu <hmathieu@etud.insa-toulouse.fr>
2021-11-23 16:22:12 +0100

    [chore] cleaning some old stuff

--- src/algorithm.ml
+++ src/algorithm.ml
@@ -4,21 +4,6 @@ open Tools
 
 type path = id list
 
-(* let find_path gr forbidden id1 id2 =
-  let arcs = out_arcs gr id1 in
-
-  let rec loop arcs forbidden path =
-    match arcs with 
-      | (id,_) when id=id2 -> Some(id2::path)
-      | [] -> forbidden
-      | (id,_)::reste -> loop reste (id::forbidden) (id::path)
-  in
-
-  loop arcs forbidden []
-  (* find_path *)
-;;
-   *)
-
 let rec find_path gr forbidden id1 id2  =
   let arcs = out_arcs gr id1 in
   
