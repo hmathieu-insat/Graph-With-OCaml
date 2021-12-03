24fcca810497930058c80e37dacdd26bd831bb56
HeineKayn <thomashoger@hotmail.fr>
2021-12-03 11:31:30 +0100

    plus propre

---
+++ src/hackparser.mli
@@ -0,0 +1,6 @@
+type hacker_record  = {id:int;nom:string;litsid:int list}
+type lit_record     = {id:int;nom:string;flow:int}
+type structure      = {hackers:hacker_record list; lits:lit_record list}
+
+(* Values are read and put in a struct *)
+val from_file: path -> structure
