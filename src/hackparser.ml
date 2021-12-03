fa2fe305d4a996e4f899ac64ac8d07a23c4d06ac
HeineKayn <thomashoger@hotmail.fr>
2021-12-03 11:36:33 +0100

    space

--- src/hackparser.ml
+++ src/hackparser.ml
@@ -1,6 +1,6 @@
-type hacker_record  = {idh:int;nomh:string;litsid:int list}
-type lit_record     = {idl:int;noml:string;capa:int}
-type structure      = {hackers:hacker_record list; lits:lit_record list}
+type hacker_record  = {idh : int; nomh : string; litsid : int list}
+type lit_record     = {idl : int; noml : string; capa : int}
+type structure      = {hackers : hacker_record list; lits : lit_record list}
 
 let empty_structure = {[];[]}
 
--- src/hackparser.mli
+++ src/hackparser.mli
@@ -1,6 +1,6 @@
-type hacker_record  = {id:int;nom:string;litsid:int list}
-type lit_record     = {id:int;nom:string;flow:int}
-type structure      = {hackers:hacker_record list; lits:lit_record list}
+type hacker_record  = {idh : int; nomh : string; litsid : int list}
+type lit_record     = {idl : int; noml : string; capa : int}
+type structure      = {hackers : hacker_record list; lits : lit_record list}
 
 (* Values are read and put in a struct *)
 val from_file: path -> structure
